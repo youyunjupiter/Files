c281c17b66c611335044c8aa11f703a9ca0f7628
youyunjupiter <youyunjupiter@gmail.com>
2016-08-29 18:03:28 +0800

    Add files via upload

--- TextMining.R
+++ TextMining.R
@@ -1,5 +1,8 @@
 Needed <- c("tm", "SnowballCC", "RColorBrewer", "ggplot2", "wordcloud", "biclust", "cluster", "igraph", "fpc")   
 install.packages(Needed, dependencies=TRUE) 
+Needed2 <- c("RTextTools")   
+install.packages(Needed2, dependencies=TRUE) 
+
 setwd("D:\\MouZhiHui\\Learning\\R\\textmining");
 TEXTFILE = "pg100.txt"
 download.file("http://www.gutenberg.org/cache/epub/100/pg100.txt", destfile = TEXTFILE)
@@ -94,25 +97,114 @@ DTM <- DocumentTermMatrix(doc.corpus)
 DTM1 <- DocumentTermMatrix(doc.corpus,control=list(weighting=weightTfIdf, minWordLength=2, minDocFreq=5))
 
 
-Needed2 <- c("RTextTools")   
-install.packages(Needed2, dependencies=TRUE) 
 library(RTextTools)
 
 #http://www.svm-tutorial.com/2014/11/svm-classify-text-r/
 #https://journal.r-project.org/archive/2013-1/collingwood-jurka-boydstun-etal.pdf
 
 test = c(rep(1,90),rep(0,92))
-DTM.common_test = cbind(DTM.common,test)
-DTM.common_test2 = create_matrix(DTM.common_test)
+shakespeare_test = cbind(shakespeare,test)
+shakespeare_test2 = create_matrix(shakespeare_test)
 
 # Configure the training data
-container <- create_container(DTM.common_test2, DTM.common_test[,62], trainSize=1:150, virgin=FALSE)
+container <- create_container(shakespeare_test2, shakespeare_test[,2], trainSize=1:160,virgin=FALSE)
 # train a SVM Model
 model <- train_model(container, "SVM", kernel="linear", cost=1)
 # new data
-predictionData <- list("love")
+#predictionData <- list("love")
+predictionData <- shakespeare[1:22]
 # create a prediction document term matrix
-predMatrix <- create_matrix(predictionData, originalMatrix=DTM.common_test2)
+predMatrix <- create_matrix(predictionData, originalMatrix=shakespeare_test2)
+# create the corresponding container
+predSize = length(predictionData);
+predictionContainer <- create_container(predMatrix, labels=rep(0,predSize), testSize=1:predSize, virgin=FALSE)
+# predict
+results <- classify_model(predictionContainer, model)
+results
+
+##################
+trace("create_matrix",edit=T)
+
+###################
+data(USCongress)
+
+# CREATE THE DOCUMENT-TERM MATRIX
+doc_matrix<- DocumentTermMatrix(
+  Corpus(VectorSource(USCongress$text)), 
+  control = list(
+   wordLengths = c(3, Inf), 
+   stopwords=TRUE, 
+   removeNumbers=TRUE, 
+   stemming=TRUE,
+   removePunctuation = TRUE
+  )
+)
+#Method 2: Not working due to NA
+#doc_matrix <- create_matrix(USCongress$text, language="english", minDocFreq=1, maxDocFreq=Inf, 
+#minWordLength=3, maxWordLength=Inf, ngramLength=1, originalMatrix=NULL, removeNumbers=TRUE, removePunctuation=TRUE, removeSparseTerms=0.1, 
+#removeStopwords=TRUE,  stemWords=TRUE, stripWhitespace=TRUE, toLower=TRUE, weighting=weightTf)
+
+container <- create_container(doc_matrix, USCongress$major, trainSize=1:4000,
+testSize=4001:4449, virgin=FALSE)
+
+SVM <- train_model(container,"SVM")
+GLMNET <- train_model(container,"GLMNET")
+MAXENT <- train_model(container,"MAXENT")
+SLDA <- train_model(container,"SLDA")
+BOOSTING <- train_model(container,"BOOSTING")
+BAGGING <- train_model(container,"BAGGING")
+RF <- train_model(container,"RF")
+NNET <- train_model(container,"NNET")
+TREE <- train_model(container,"TREE")
+
+SVM_CLASSIFY <- classify_model(container, SVM)
+GLMNET_CLASSIFY <- classify_model(container, GLMNET)
+MAXENT_CLASSIFY <- classify_model(container, MAXENT)
+SLDA_CLASSIFY <- classify_model(container, SLDA)
+BOOSTING_CLASSIFY <- classify_model(container, BOOSTING)
+BAGGING_CLASSIFY <- classify_model(container, BAGGING)
+RF_CLASSIFY <- classify_model(container, RF)
+NNET_CLASSIFY <- classify_model(container, NNET)
+TREE_CLASSIFY <- classify_model(container, TREE)
+
+analytics <- create_analytics(container,
+cbind(SVM_CLASSIFY, GLMNET_CLASSIFY))
+summary(analytics)
+
+analytics <- create_analytics(container,
+cbind(SVM_CLASSIFY, SLDA_CLASSIFY,
+BOOSTING_CLASSIFY, BAGGING_CLASSIFY,
+RF_CLASSIFY, GLMNET_CLASSIFY,
+NNET_CLASSIFY, TREE_CLASSIFY,
+MAXENT_CLASSIFY))
+summary(analytics)
+
+# CREATE THE data.frame SUMMARIES
+topic_summary <- analytics@label_summary
+alg_summary <- analytics@algorithm_summary
+ens_summary <-analytics@ensemble_summary
+doc_summary <- analytics@document_summary
+
+#Ensemble agreement
+create_ensembleSummary(analytics@document_summary)
+
+#Cross Validation
+SVM <- cross_validate(container, 4, "SVM")
+GLMNET <- cross_validate(container, 4, "GLMNET")
+MAXENT <- cross_validate(container, 4, "MAXENT")
+SLDA <- cross_validate(container, 4, "SLDA")
+BAGGING <- cross_validate(container, 4, "BAGGING")
+BOOSTING <- cross_validate(container, 4, "BOOSTING")
+RF <- cross_validate(container, 4, "RF")
+NNET <- cross_validate(container, 4, "NNET")
+TREE <- cross_validate(container, 4, "TREE")
+
+write.csv(analytics@document_summary, "DocumentSummary.csv")
+
+
+
+
+
 
 
 
